d0e32e75e6ea332f01cb98d16c71cdded94fa56b
Kriti Shukla <kshukla101@gmail.com>
2025-10-15 22:41:36 -0400

    more things to make static

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
+export const revalidate = false;
+export const runtime = "nodejs";
 
 import { useSearchParams } from "next/navigation";
 import nextDynamic from "next/dynamic";
