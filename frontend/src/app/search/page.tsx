c8fb429a906adcec01f1500fe3f5b041d936a84a
Kriti Shukla <kshukla101@gmail.com>
2025-10-15 23:01:05 -0400

    fixing search header

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
-export const revalidate = false;
-export const runtime = "nodejs";
 
 import { useSearchParams } from "next/navigation";
 import nextDynamic from "next/dynamic";
